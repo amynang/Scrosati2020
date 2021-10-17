368bc6ba06a2b743c83ec2a2dcf94839b146144b
Angelos Amyntas <angelos.amyntas@idiv.de>
2021-10-17 18:40:45 +0200

    globi does what worms cannot

--- Scrosati2020.R
+++ Scrosati2020.R
@@ -1,10 +1,39 @@
 rm(list=ls())
 library(readxl)
 library(worrms)
+library(rglobi)
+library(stringr)
+library(igraph)
 
 data=read_excel("H:/Literature/Scrosati2020_DataS1.xlsx",
                 sheet = "Sheet1",
                 na="")
+data=as.data.frame(data)
+
+taxa = data[,3]
+
+taxa = taxa %>% str_replace(" sp.*", "")
+
+mat = get_interaction_matrix(taxa, 
+                             taxa, "eats")
+matr = as.matrix(mat[,-1])
+rownames(matr) = taxa
+colnames(matr) = rownames(matr)
+
+g=graph_from_adjacency_matrix(matr, mode = "directed")
+which(degree(g)==0)
+
+plot(g, 
+     layout=layout_in_circle,
+     edge.arrow.size=.1,
+     vertex.label.color="black",
+     main = "bla")
+
+tkplot(g, 
+       layout=layout_in_circle,
+       edge.arrow.size=.1,
+       vertex.label.color="black",
+       main = "bla")
 
 
 try = wm_records_name(name = "Cirratulus cirratus")
